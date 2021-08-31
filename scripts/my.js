e3ef87903863e40f268cc867f81797f57475dff7
Kiều Lê <88967495+kieule-dev@users.noreply.github.com>
2021-08-31 18:25:06 +0700

    Add files via upload

---
+++ scripts/my.js
@@ -0,0 +1,16 @@
+
+
+
+
+arrowElement = document.querySelector('.ti-angle-double-up');
+
+setInterval(function () {
+    arrowElement.classList.toggle('up')
+}, 500);
+
+function scrollWin(x, y) {  
+    window.scrollTo(x, y);
+   
+};
+
+
